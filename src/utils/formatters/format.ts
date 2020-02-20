36f8ea76b2ca192aeccabe231aedf5ac4a03ef03
John Doe <bulk.yard@protonmail.com>
2020-02-20 15:57:18 +0000

    Remove space from negative values formatting

--- src/utils/formatters/format.ts
+++ src/utils/formatters/format.ts
@@ -44,7 +44,7 @@ export function formatAsShorthandNumbers(amount: BigNumber, precision?: number):
 
 export function formatCryptoBalance(amount: BigNumber): string {
   if (amount.absoluteValue().lt(oneThousandth)) {
-    return `${amount.isNegative() ? '- 0.000' : '<0.001'}`;
+    return `${amount.isNegative() ? '-0.000' : '<0.001'}`;
   }
 
   if (amount.absoluteValue().lt(ten)) {
