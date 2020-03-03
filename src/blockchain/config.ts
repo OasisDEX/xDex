2cc4ec6a62b80c2c3994e14c9b636dae9f62af2f
Maciek Kamiński <aundumla@gmail.com>
2020-03-03 14:56:11 +0100

    OPLA address uptaded to the latest version

--- src/blockchain/config.ts
+++ src/blockchain/config.ts
@@ -440,7 +440,7 @@ const kovan: NetworkConfig = {
     return load(proxyCreationAndExecute, '0xee419971e63734fed782cfe49110b1544ae8a773');
   },
   get proxyActions() {
-    return load(proxyActions, '0xc455c5c1cc9adf812d10d1589e9a63a55185d9aa');
+    return load(proxyActions, '0x59bf9aea32391e6ad095d00461fbc34d000dbb18');
   },
   get migration() {
     return '0x411b2faa662c8e3e5cf8f01dfdae0aee482ca7b0';
