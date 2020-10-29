d04a9c49c65ea49f35dd00e4d8535078d382e6f9
Jeannette <jeannette@makerdao.com>
2020-10-29 08:05:14 -0700

    Specify AWS profile

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -14,6 +14,6 @@ aws configure set default.output json
 aws configure set aws_access_key_id $STAGING_AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $STAGING_AWS_SECRET_ACCESS_KEY
 
-aws s3 sync ./build s3://$STAGING_AWS_BUCKET_NAME/trade/ --delete
-aws cloudfront create-invalidation --distribution-id $STAGING_AWS_CF_ID --paths "/*"
+aws --profile default s3 sync ./build s3://$STAGING_AWS_BUCKET_NAME/trade/ --delete
+aws --profile default cloudfront create-invalidation --distribution-id $STAGING_AWS_CF_ID --paths "/*"
 
