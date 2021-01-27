9320713c92fda804ebe94e5cace35fcb874d517b
Kuba Włodarczyk <fenris85@gmail.com>
2021-01-27 10:55:01 +0100

    OTC contract update banner

--- src/landingPage/Banner.tsx
+++ src/landingPage/Banner.tsx
@@ -3,7 +3,9 @@
  */
 
 import * as classnames from 'classnames';
-import * as React from 'react';
+import React, { useState } from 'react';
+import { SvgImage } from 'src/utils/icons/utils';
+import closeSvg from '../icons/close.svg';
 import { Button } from '../utils/forms/Buttons';
 import * as styles from './Banner.scss';
 
@@ -12,12 +14,15 @@ interface BannerProps {
   content: string | React.ReactNode;
   continue?: () => any;
   theme: 'success' | 'danger' | 'warning';
+  isClosable?: boolean;
 }
 
 export const Banner = (props: BannerProps) => {
-  const { content, continue: onContinue, buttonLabel, theme } = props;
+  const { content, continue: onContinue, buttonLabel, theme, isClosable } = props;
 
-  return (
+  const [isBannerHidden, setIsBannerHidden] = useState(false);
+
+  return !isBannerHidden ? (
     <section className={styles.section}>
       <div className={classnames(styles.panel, styles[theme])}>
         <div>{content}</div>
@@ -26,7 +31,15 @@ export const Banner = (props: BannerProps) => {
             {buttonLabel}
           </Button>
         )}
+        {isClosable && (
+          <div onClick={() => setIsBannerHidden(true)} style={{ position: 'absolute', top: '5px', right: '5px' }}>
+            <SvgImage
+              image={closeSvg}
+              style={{ width: '24px', display: 'block', cursor: 'pointer', fill: '#546978' }}
+            />
+          </div>
+        )}
       </div>
     </section>
-  );
+  ) : null;
 };
