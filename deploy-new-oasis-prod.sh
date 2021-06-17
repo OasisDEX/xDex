fbd3a07daab0670b49614fcec8b20da0db7e3c39
Jeannette <jeannette@makerdao.com>
2021-06-17 18:33:38 -0500

    Fix env vars

--- deploy-new-oasis-prod.sh
+++ deploy-new-oasis-prod.sh
@@ -15,9 +15,9 @@ export PATH=$HOME/.local/bin:$PATH
 
 aws configure set default.region $OASIS_AWS_REGION
 aws configure set default.output json
-aws configure set aws_access_key_id $OASIS_AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $OASIS_AWS_SECRET_ACCESS_KEY
+aws configure set aws_access_key_id $OASIS_PROD_AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $OASIS_PROD_AWS_SECRET_ACCESS_KEY
 
-aws --profile default s3 sync ./build s3://$OASIS_AWS_BUCKET_NAME/trade/ --delete
-aws --profile default cloudfront create-invalidation --distribution-id $OASIS_AWS_CF_ID --paths "/*"
+aws --profile default s3 sync ./build s3://$OASIS_PROD_AWS_BUCKET_NAME/trade/ --delete
+aws --profile default cloudfront create-invalidation --distribution-id $OASIS_PROD_AWS_CF_ID --paths "/*"
 
